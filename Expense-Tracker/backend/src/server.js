bc23201a2a7a9611c2fe6c5b5a5da42c94288430
Ronit178693 <ronitagrawal9921@gmail.com>
2026-02-10 23:28:55 +0530

    Fixed deployment issue

--- Expense-Tracker/backend/src/server.js
+++ Expense-Tracker/backend/src/server.js
@@ -13,7 +13,7 @@ dotenv.config();
 const app = express(); // Initialize express app
 
 app.use(cors({
-    origin: process.env.CLIENT_URL || "*",
+    origin: [process.env.CLIENT_URL, "http://localhost:5173"],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }))
